dcd16bd9e2108a02cb3488f464e19e4f6c4b4b0a
Hugo Regadas <dev.hugoregadas@gmail.com>
2022-03-15 16:53:00 +0100

    Designing Cart Page

--- src/app/pages/tabs/cart/cart.page.ts
+++ src/app/pages/tabs/cart/cart.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +10,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPage implements OnInit {
 
-  constructor() { }
+  urlCheck: any;
+  url: any;
+  model: any = {};
+  deliveryCharge = 20;
+  instruction: any;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.checkUrl();
+    this.getmodel();
+  }
+
+  getCart() {
+   return Plugins.Storage.get({key: 'cart'});
+  }
+
+  async getmodel() {
+    let data: any = await this.getCart();
+    if(data?.value) {
+      this.model = await JSON.parse(data.value);
+      console.log(this.model);
+      this.calculate();
+    }
+  }
+
+  async calculate() {
+    let item = this.model.items.filter(x => x.quantity > 0);
+    this.model.items = item;
+    this.model.totalPrice = 0;
+    this.model.totalItem = 0;
+    this.model.deliveryCharge = 0;
+    this.model.grandTotal = 0;
+    item.forEach(element => {
+      this.model.totalItem += element.quantity;
+      this.model.totalPrice += (parseFloat(element.price) * parseFloat(element.quantity));
+    });
+    this.model.deliveryCharge = this.deliveryCharge;
+    this.model.totalPrice = parseFloat(this.model.totalPrice).toFixed(2);
+    this.model.grandTotal = (parseFloat(this.model.totalPrice) + parseFloat(this.model.deliveryCharge)).toFixed(2);
+    if(this.model.totalItem == 0) {
+      this.model.totalItem = 0;
+      this.model.totalPrice = 0;
+      this.model.grandTotal = 0;
+      await this.clearCart();
+      this.model = null;
+    }
+    console.log('cart: ', this.model);
+  }
+
+  clearCart() {
+    return Plugins.Storage.remove({key: 'cart'});
+  }
+
+  checkUrl() {
+    let url: any = (this.router.url).split('/');
+    console.log('url: ', url);
+    const spliced = url.splice(url.length - 2, 2); // /tabs/cart url.length - 1 - 1
+    this.urlCheck = spliced[0];
+    console.log('urlcheck: ', this.urlCheck);
+    url.push(this.urlCheck);
+    this.url = url;
+    console.log(this.url);
+  }
+
+  getPreviousUrl() {
+    return this.url.join('/');
+  }
+
+  quantityPlus(index) {}
+
+  quantityMinus(index) {}
+
+  addAddress() {}
+
+  changeAddress() {}
+
+  makePayment() {
+    console.log('make payment');
   }
 
 }
